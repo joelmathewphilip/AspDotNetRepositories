f3c3bb6a208ad3e0b4714f2dffb5076c8f9de0d3
joelmathewphilip <joelmathew2809@gmail.com>
2022-09-12 10:07:12 +0530

    Add Data Classes, Entity Class, removed default controllers and classes

---
+++ src/Services/Catalog/Catalog.API/Data/CatalogContext.cs
@@ -0,0 +1,22 @@
+﻿using Catalog.API.Entities;
+using MongoDB.Driver;
+
+namespace Catalog.API.Data
+{
+    //create mongo connection and seed data
+    public class CatalogContext : ICatalogContext
+    {
+        public CatalogContext(IConfiguration configuration)
+        {
+            var client = new MongoClient(configuration.GetValue<string>("DatabaseSettings:ConnectionString"));
+
+            //to create a database when not it does not have one
+            var database = client.GetDatabase(configuration.GetValue<string>("DatabaseSettings:DatabaseName"));
+
+            Products = database.GetCollection<Product>(configuration.GetValue<string>("DatabaseSettings:CollectionName"));
+
+            //CatalogContextSeed.SeedData(Products);
+        }
+       public IMongoCollection<Product> Products {get;}
+    }
+}
